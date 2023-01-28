0c1374fb4e4f635bfc707fd3626ef3ad9500fe72
Benjamin DIDIER <contact@aziks.aleeas.com>
2023-01-28 19:30:24 +0100

    feat: add string table parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,6 +3,8 @@ mod console_command;
 mod header;
 mod user_command;
 
+pub mod string_table;
+
 // Re-export them to be in `csgo_demo_parser::parser`
 pub use command::PacketHeader;
 pub use header::DemoHeader;
